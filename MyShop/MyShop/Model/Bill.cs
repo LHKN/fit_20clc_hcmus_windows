f8773b8c0a74cb55c08982501e11f862fe6291a8
MiwaUS7605 <minhquang9802@gmail.com>
2023-04-04 18:22:13 +0700

    feat: project now using WinUI3 Library

---
+++ MyShop/MyShop/Model/Bill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Model
+{
+    public sealed class Bill
+    {
+        private int _id;
+        private int _customerId;
+        private int _totalPrice;
+        private DateTime _transactionDate;
+
+        public int Id { get => _id; set => _id = value; }
+        public int CustomerId { get => _customerId; set => _customerId = value; }
+        public int TotalPrice { get => _totalPrice; set => _totalPrice = value; }
+        public DateTime TransactionDate { get => _transactionDate; set => _transactionDate = value; }
+    }
+}
---
+++ MyShop/MyShop/Model/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Model
+{
+    public sealed class Genre
+    {
+        private int _id;
+        private string _name;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+    }
+}
