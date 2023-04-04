be354e497ad5aa73bdf0a0365e47b0dae87406ad
MiwaUS7605 <minhquang9802@gmail.com>
2023-04-04 18:22:13 +0700

    feat: project now using WinUI3 Library

---
+++ MyShop/MyShop/Model/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Model
+{
+    public enum Role
+    {
+        Customer, Admin
+    }
+    public class Account
+    {
+        private int _id;
+        private string _name;
+        private string _phoneNumber;
+        private string _email;
+        private string _password;
+        private Role _role;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Password { get => _password; set => _password = value; }
+        public Role Role { get => _role; set => _role = value; }
+    }
+}
---
+++ MyShop/MyShop/Model/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Model
+{
+    public sealed class Book
+    {
+        private int _id;
+        private string _title;
+        private string _author;
+        private string _description;
+        private DateTime _publishedDate;
+        private int _gerneId;
+        private int _price;
+        private int _quantity;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Title { get => _title; set => _title = value; }
+        public string Author { get => _author; set => _author = value; }
+        public string Description { get => _description; set => _description = value; }
+        public DateTime PublishedDate { get => _publishedDate; set => _publishedDate = value; }
+        public int GerneId { get => _gerneId; set => _gerneId = value; }
+        public int Price { get => _price; set => _price = value; }
+        public int Quantity { get => _quantity; set => _quantity = value; }
+    }
+}
