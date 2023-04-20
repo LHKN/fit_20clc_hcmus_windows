cdd7064a343e10a6af01ae18f5bc88d58e27b396
LHKN <79393909+LHKN@users.noreply.github.com>
2023-04-20 16:49:24 +0700

    fix: binding toggle switch

--- MyShop/MyShop/ViewModel/SettingViewModel.cs
+++ MyShop/MyShop/ViewModel/SettingViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using MyShop.Model;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyShop.ViewModel
 {
-    class SettingViewModel: ViewModelBase
+    public class SettingViewModel: ViewModelBase
     {
+        // Fields
+        private bool _toggleSwitchIsOn;
+
+        // getter, setter
+        public bool ToggleSwitchIsOn { get => _toggleSwitchIsOn; set => _toggleSwitchIsOn = value; }
+
+        //-> Commands
+
+        // Constructor
+        public SettingViewModel()
+        {
+            if (_toggleSwitchIsOn == true)
+            {
+                var sysconfig = System.Configuration.ConfigurationManager.OpenExeConfiguration(
+                    ConfigurationUserLevel.None);
+                //sysconfig.AppSettings.Settings["RememberPage"].Value = currentPage;
+            }
+        }
+
     }
 }
