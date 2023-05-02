436999a2fea1eadf21cd8bea17042807da3cacdd
LHKN <79393909+LHKN@users.noreply.github.com>
2023-05-02 19:57:49 +0700

    chore: create MyImage and MyText

---
+++ Paint/Paint/MyText.cs
@@ -0,0 +1,46 @@
+﻿using MyContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Paint
+{
+    public class MyText
+    {
+        public string Name => "Text";
+
+        public string Content { get; set; }
+        public bool IsFocusable { get; set; }
+
+        public FontFamily FontFamily { get; set; }
+        public int FontSize { get; set; }
+        public FontWeight FontWeight { get; set; }
+        public FontStyle FontStyle { get; set; }
+        public TextDecorationCollection TextDecorations { get; set; }
+        Point Start { get; set; }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UIElement Draw(Color color, int thickness, DoubleCollection stroke)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEnd(Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStart(Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
